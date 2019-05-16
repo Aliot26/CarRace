82728f737d9c5b60cfb570fec5089f8c14e17347
Aliot26 <olgakochanowa@gmail.com>
2019-05-16 14:50:39 +0200

    created weather class with methods

---
+++ src/com/codecool/car_race/Weather.java
@@ -0,0 +1,21 @@
+package com.codecool.car_race;
+
+import java.util.Random;
+
+
+public class Weather {
+    private boolean isRaining;
+
+    public Weather() {
+        setRaining();
+    }
+
+    public boolean getRain() {
+        return isRaining;
+    }
+
+    private void setRaining() {
+        Random r = new Random();
+        isRaining = r.nextDouble() <= 0.3;
+    }
+}
